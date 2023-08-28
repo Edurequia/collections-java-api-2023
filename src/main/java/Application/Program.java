8ab79b0b029072b1453d515429a3869284cff68f
Edurequia <edurequia@gmail.com>
2023-08-28 14:39:28 -0300

    resolvendo exercicio ListaTarefa
    Signed-off-by: Edurequia <edurequia@gmail.com>

---
+++ src/main/java/Application/Program.java
@@ -0,0 +1,37 @@
+package main.java.Application;
+
+import main.java.list.OperacoesBasicas.CarrinhoDeCompras;
+import main.java.list.OperacoesBasicas.ListaTarefa;
+
+public class Program {
+    public static void main(String[] args) {
+
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        System.out.println("O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 2");
+        System.out.println("O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+
+        System.out.println("---------------CARRINHO DE COMPRAS------------------");
+
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        carrinhoDeCompras.adicionarItem("Computador", 2000.0, 3);
+        carrinhoDeCompras.adicionarItem("Teclado", 300.0, 2);
+        carrinhoDeCompras.adicionarItem("Mouse", 150.0, 5);
+        carrinhoDeCompras.exibirItens();
+
+        carrinhoDeCompras.removerItem("teclado");
+        carrinhoDeCompras.exibirItens();
+
+
+        System.out.println("O valor total dos itens é: " + carrinhoDeCompras.calcularValorTotal());
+
+    }
+}
---
+++ src/main/java/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -0,0 +1,40 @@
+package main.java.list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+
+    private List<Item> itens;
+
+    public CarrinhoDeCompras() {
+        this.itens = new ArrayList<>();
+    }
+
+    public void adicionarItem(String name, double price, int quantity){
+        itens.add(new Item(name, price, quantity));
+    }
+
+    public void removerItem(String nome){
+        List<Item> itensParaRemover = new ArrayList<>();
+        for (Item i : itens){
+            if(i.getName().equalsIgnoreCase(nome)){
+                itensParaRemover.add(i);
+            }
+        }
+        itens.removeAll(itensParaRemover);
+    }
+
+    public double calcularValorTotal(){
+        double valorTotal = 0.0;
+        for (Item i : itens){
+            valorTotal += i.getQuantity() * i.getPrice();
+        }
+        return valorTotal;
+    }
+
+    public void exibirItens(){
+        System.out.println(itens);
+    }
+
+}
